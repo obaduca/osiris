68e6f8331b43bf4addd017dcc39f638ef51d95b0
obaduca <113936837+obaduca@users.noreply.github.com>
2022-10-15 15:02:05 +0100

    Update osiris.lua

--- osiris.lua
+++ osiris.lua
@@ -2,7 +2,7 @@ repeat
     task.wait()
 until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 
-if game.PlaceId == "9825515356" then
+if game.PlaceId == 9825515356 then
     printconsole("hc")
   loadstring(game:HttpGet('https://scripts.luawl.com/12862/OsirisHC.lua'))()
 else
