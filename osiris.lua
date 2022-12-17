02d9d46229e683057727685c0d6f03be3531b167
obaduca <113936837+obaduca@users.noreply.github.com>
2022-12-17 19:07:25 +0000

    Update osiris.lua

--- osiris.lua
+++ osiris.lua
@@ -6,7 +6,7 @@ if game.PlaceId == 9825515356 then
     loadstring(game:HttpGet('https://scripts.luawl.com/12862/OsirisHC.lua'))()
 elseif game.PlaceId == 9183932460 then
     loadstring(game:HttpGet('https://scripts.luawl.com/13007/UntitledHoodOsiris.lua'))()
-elseif game.PlaceId == 5602055394 then
+elseif game.PlaceId == 5602055394 or game.PlaceId == 9293553078 then
     loadstring(game:HttpGet('https://scripts.luawl.com/13041/DaHoodModded.lua'))()
 elseif game.PlaceId == 9824221333 then
     loadstring(game:HttpGet('https://scripts.luawl.com/13219/DaHoodAimTrainer.lua'))()
